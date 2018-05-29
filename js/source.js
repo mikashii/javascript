91ba0b71c5b5c561a6b264d516a7730c4c5a74bc
mikashii <carolina.portas@outlook.com>
2018-05-29 16:33:25 -0300

    Arquivos separados, adicionada file com o código javascript

---
+++ js/source.js
@@ -0,0 +1,7 @@
+// Folha com as funções do JavaScript
+
+function soma(){
+  const x = parseFloat(prompt('Digite o valor de x'))
+  const y = parseFloat(prompt('Digite o valor de y'))
+    let result = x + y
+};
