aad4a74e92fec7e4eb11a4810e1c445548e3379d
mikashii <carolina.portas@outlook.com>
2018-06-03 15:13:56 -0300

    01

---
+++ exercicios/usandoVar1.js
@@ -0,0 +1,11 @@
+{{{{{var sera = 'Sera?'}}}}}
+
+console.log(sera)
+
+// Uma variavel definida com VAR, só tem dois escopos possíveis
+
+function teste(){
+    var local =123
+}
+
+console.log(local)
