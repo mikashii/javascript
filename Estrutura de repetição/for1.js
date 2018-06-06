2c485a4797112ffe3ea3b250cd83587b2ef75e47
mikashii <carolina.portas@outlook.com>
2018-06-06 16:40:07 -0300

    Template String

--- Estrutura de repetição/for1.js
+++ Estrutura de repetição/for1.js
@@ -4,5 +4,13 @@ while (contador <= 10){
   contador++
 }
 
+/*
 for (let i = 1; i <= 10; i++)
-console.log('O contador é ' + i)
+console.log(`O contador é ${i}`)
+*/
+
+
+let numero2 = 4
+let numero4 = 'quatro'
+
+console.log(`\n${numero2} não é ${numero4}`)
