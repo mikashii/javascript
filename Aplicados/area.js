a48ae325e795cbd06b72d1bf0021096c0595f02a
mikashii <carolina.portas@outlook.com>
2018-06-07 23:23:57 -0300

    Calculos adicionados

---
+++ Aplicados/area.js
@@ -0,0 +1,11 @@
+function area (valor1, valor2, valor3){
+    const triangulo = valor1 * valor3 / 2
+    const raio = Math.pow(valor3,2) * 3.14159
+    const trapezio = valor1 + valor2 / 2 * valor3
+
+    return console.log(`Triangulo: ${triangulo.toFixed(3)}\nCirculo: ${raio.toFixed(3)}\nTrapézio: ${trapezio.toFixed(3)}`)
+}
+
+
+area(3.0, 4.0, 5.2)
+//3.0 4.0 5.2
---
+++ Aplicados/calculo_media.js
@@ -0,0 +1,20 @@
+function calcular_media(n1, n2) {
+    let media = (n1 * 3.5 + n2 * 7.5) / (3.5 + 7.5)
+
+    return console.log(media)
+}
+calcular_media(5.0, 7.1)
+calcular_media(0.0, 7.1)
+calcular_media(10.0, 10.0)
+
+
+// Função de calculo de três valores
+
+
+function media(n1, n2, n3) {
+    let media = (n1 * 2 + n2 * 3 + n3 * 5) / (2 + 3 + 5)
+    return console.log(`\nA media de três valores é ${media}`)
+}
+
+media(5.0, 6.0, 7.0)
+
