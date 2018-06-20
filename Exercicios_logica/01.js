33eea410eda7351c05205a6f6913526b712502e5
mikashii <carolina.portas@outlook.com>
2018-06-20 16:17:27 -0300

    Objetos e métodos

---
+++ Exercicios_logica/01.js
@@ -0,0 +1,4 @@
+let total = (4/2) + (2/4)
+let total2 = 4/2 + 2/4
+
+console.log(total,total2)
--- exercicios/objeto.js
+++ exercicios/objeto.js
@@ -1,10 +1,14 @@
-const prod1 = {} // Uma array vazia, abaixo são adicionadas as chaves e os valores
+// Objetos são um conjunto de chave valor
+
+const prod1 = {}        // Uma objeto vazio, abaixo são adicionadas as chaves e os valores
 prod1.nome = 'Celular Ultra Mega'
 prod1.preco = 4000
-prod1['Desconto Legal'] = 0.40 // Evitar atributos com espaço
+prod1['Desconto Legal'] = 0.40    //     Evitar atributos com espaço
+
+// prod1 é a objeto / nome é a chave e 'Celular ultra mega é o valor 
+
 
 // outra forma de sentença para criar a array
-
 
 const Carolina = {
   Nome: "Carolina",
@@ -13,15 +17,63 @@ const Carolina = {
   job: "Web developer",
   isMarried: false
 }
-console.log(Carolina) // exibe todo  o conteúdo do objeto
-console.log(["job"]) // exibe a key 'job'. É necessário passar a chave como uma string
+console.log(Carolina)                  // exibe todo  o conteúdo do objeto
+console.log(["job"])                    // exibe a key 'job'. É necessário passar a chave como uma string
+//
 
 
-console.log(["lastName"])
-const trabalho = 'job'  //cria uma variavel a partir da key do objeto "Carolina"
-const sobreNome = 'lastName'    // 
-
-
-console.log(Carolina[trabalho]) // atribui um valor a variável 'trabalho' vindo do objeto 'Carolina' usa-se os brackets
+console.log(["lastName"])          // criar variável através da chave 'lastName'
+const trabalho = 'job'                 //cria uma variavel a partir da key do objeto "Carolina"
+const sobreNome = 'lastName'            // 
+console.log(Carolina[trabalho])         // atribui um valor a variável 'trabalho' vindo do objeto 'Carolina' usa-se os brackets
 console.log(Carolina[sobreNome])
-//                     variavel 
+//                   variavel 
+
+/** Data mutation */
+
+Carolina.lastName = 'Cabral';
+Carolina['job'] = 'Web Developer'
+Carolina.isMarried = true;
+console.log(Carolina);
+
+    // Criar uma função dentro de um objeto.
+/*
+let Lais = {
+  nome: 'Láis',
+  sobreNome: 'Banhidai',
+  yearOfBirth: '1999',
+  job: 'Aux administrativo',
+  isMarried: false,
+  family: ["Carol", "Bete"],
+  calculateAge: function (){
+    return 2018 - this.yearOfBirth;
+  }
+  //   console.log(Lais.calculateAge());  // Chamando o método
+  
+  // Atribuir o valor a uma variavel
+  const age = Lais.calculateAge(); // vinculando os valores da função
+  Lais.age = age; // criando uma nova chave chamada "age" com o valor da função calculateAge()
+  console.log(Lais.age)
+  console.log(Lais)
+}; 
+
+*/
+
+/**
+ * Versão dois
+ */
+let Lais = {
+  nome: 'Láis',
+  sobreNome: 'Banhidai',
+  yearOfBirth: '1999',
+  job: 'Aux administrativo',    // cada chave > valor é chamado de propriedade do objeto
+  isMarried: false,
+  family: ["Carol", "Bete"],
+  calculateAge: function (){
+   // return 2018 - this.yearOfBirth; // versão 1
+   this.age = 2018 - this.yearOfBirth;
+  }
+};
+
+Lais.calculateAge()
+console.log(Lais)
