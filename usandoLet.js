0116676ad5ff910899e7ae2c7463a454fffd8c66
mikashii <carolina.portas@outlook.com>
2019-06-26 15:32:57 -0300

    Variáveis LET

---
+++ usandoLet.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+/**Variáveis com let, funcionam somente dentro do escopo das mesmas */
+
+console.log('fora = ', numero, ', Let funciona apenas dentro do escopo onde foi declarada')
